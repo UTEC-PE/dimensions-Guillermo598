4049f319dea5603afa923fb3c7c173fdced422d3
Guillermo598 <38139707+Guillermo598@users.noreply.github.com>
2018-09-11 20:09:28 -0500

    Added CMake

---
+++ cmake-build-debug/CMakeFiles/dimensions-Guillermo598.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dimensions-Guillermo598.dir/main.cpp.obj"
+  "dimensions-Guillermo598.pdb"
+  "dimensions-Guillermo598.exe"
+  "dimensions-Guillermo598.exe.manifest"
+  "libdimensions-Guillermo598.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/dimensions-Guillermo598.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/dimensions-Guillermo598.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for dimensions-Guillermo598.
+# This may be replaced when dependencies are built.
